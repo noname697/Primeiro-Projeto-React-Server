2495391d4aaf1eb9a288716512ef222208570a85
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-01-15 11:20:13 -0300

    Services

--- controllers/livro.js
+++ controllers/livro.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
+const { getTodosLivros } = require("../services/livro.js");
 
 function getLivros(req, res) {
   try {
-    const livros = JSON.parse(fs.readFileSync("livros.json"));
+    const livros = getTodosLivros();
     res.send(livros);
   } catch (error) {
     res.status(500).json({ message: error.message });
