f372081bead8539145f95f1fb4e76a266949c089
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-01-14 22:13:45 -0300

    Outras rotas criadas

--- rotas/livro.js
+++ rotas/livro.js
@@ -6,4 +6,20 @@ router.get("/", (req, res) => {
   res.status(200).send("Olá mundo!");
 });
 
+router.post("/", (req, res) => {
+  res.send("Você fez uma requisição do tipo POST");
+});
+
+router.put("/", (req, res) => {
+  res.send("Você fez uma requisição do tipo PUT");
+});
+
+router.patch("/", (req, res) => {
+  res.send("Você fez uma requisição do tipo PATCH");
+});
+
+router.delete("/", (req, res) => {
+  res.send("Você fez uma requisição do tipo DELETE");
+});
+
 module.exports = router;
