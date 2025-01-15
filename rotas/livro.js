1178e3b96b85d02a76442f2e3a74b83617520ea8
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-01-14 22:19:12 -0300

    try catch

--- rotas/livro.js
+++ rotas/livro.js
@@ -3,23 +3,23 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send("Olá mundo!");
+  try {
+    res.status(200).send("Olá mundo!");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 router.post("/", (req, res) => {
-  res.send("Você fez uma requisição do tipo POST");
-});
-
-router.put("/", (req, res) => {
-  res.send("Você fez uma requisição do tipo PUT");
+  res.status(201).send("Você fez uma requisição do tipo POST");
 });
 
 router.patch("/", (req, res) => {
-  res.send("Você fez uma requisição do tipo PATCH");
+  res.status(201).send("Você fez uma requisição do tipo PATCH");
 });
 
 router.delete("/", (req, res) => {
-  res.send("Você fez uma requisição do tipo DELETE");
+  res.status(204).send("Você fez uma requisição do tipo DELETE");
 });
 
 module.exports = router;
